0ee2ee712650ee82a2ee0e985e9f8c3a27b4a176
SnowdenLozard <snowdenlozard@email.com>
2026-01-10 12:51:48 +0000

    alterções no dashboard

--- Dashboard/index.php
+++ Dashboard/index.php
@@ -10,25 +10,36 @@
 <body>
     <main>
 
-        <section class="header_lateral">
-            <img src="../imagem/fabio.jpeg" alt="imagem_perfil">
+        <section class="header">
+            <div class="header_lateral">
+                 <img src="../imagem/canudo.png" alt="imagem_perfil">
 
-            <nav class="desktop">
-                <ul>
-                    <li><a href=""><i data-lucide="megaphone"></i> Casa</a></li>
-                    <li><a href=""><i data-lucide="megaphone"></i> Cursos</a></li>
-                    <li><a href=""><i data-lucide="megaphone"></i> Certificados</a></li>
-                    <li><a href=""><i data-lucide="megaphone"></i> Ranking</a></li>
-                    <li><a href=""><i data-lucide="megaphone"></i> Projectos</a></li>
-                    <li><a href=""><i data-lucide="megaphone"></i> Educonnect</a></li>
-                    <li><a href=""><i data-lucide="megaphone"></i> Comunidade</a></li>
-                    <li><a href=""><i data-lucide="megaphone"></i> Teu</a></li>
-                    <li><a href=""><i data-lucide="megaphone"></i> Enunciado</a></li>
-                    <li><a href=""><i data-lucide="megaphone"></i> Video</a></li>    
-                    <li><a href=""><i data-lucide="megaphone"></i> Perfil</a></li>
-                </ul>
-            </nav>
+                <nav class="desktop">
+                    <ul>
+                        <li><a href=""><i data-lucide="megaphone"></i> Casa</a></li>
+                        <li><a href=""><i data-lucide="megaphone"></i> Cursos</a></li>
+                        <li><a href=""><i data-lucide="megaphone"></i> Certificados</a></li>
+                        <li><a href=""><i data-lucide="megaphone"></i> Ranking</a></li>
+                        <li><a href=""><i data-lucide="megaphone"></i> Projectos</a></li>
+                        <li><a href=""><i data-lucide="megaphone"></i> Educonnect</a></li>
+                        <li><a href=""><i data-lucide="megaphone"></i> Comunidade</a></li>
+                        <li><a href=""><i data-lucide="megaphone"></i> Teu</a></li>
+                        <li><a href=""><i data-lucide="megaphone"></i> Enunciado</a></li>
+                        <li><a href=""><i data-lucide="megaphone"></i> Video</a></li>    
+                        <li><a href=""><i data-lucide="megaphone"></i> Perfil</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+             <div class="centro_dash">
+                <div class="msg_">
+                    <h3>Bem-vindo ao Dashboard, Fábio Cassule Manuel</h3>
+                </div>
+            </div>
+           
         </section>
+
+       
     </main>
 
        <script>
